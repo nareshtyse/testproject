cd0d705f4d1ba6a167057e26fc224190f3211fdd
nareshtyse <48803828+nareshtyse@users.noreply.github.com>
2019-06-24 02:06:51 -0400

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,1 +1,4 @@
- aws ec2 run-instances --image-id ami-0c55b159cbfafe1f0 --count 1 --instance-type t2.micro --key-name Jenkins --subnet-id subnet-928e2dde
+sh label: '', script: '''export AWS_ACCESS_KEY_ID=AKIAJIPQ5XA3ND46KYHQ
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-2
+aws ec2 run-instances --image-id ami-0c55b159cbfafe1f0 --count 1 --instance-type t2.micro --key-name Jenkins --subnet-id subnet-928e2dde'''
